145693fcd0d0947b083aeb750ac8a30c640aecf8
Alex Seisler <aseisler7045@stevenscollege.edu>
2025-06-16 16:50:29 -0400

    remove federation env and add database url

--- settings.py
+++ settings.py
@@ -4,12 +4,8 @@ import os
 class Database:
     def __init__(self):
         self.conn = psycopg2.connect(
-            dbname=os.getenv("FEDERATION_DB_NAME"),
-            user=os.getenv("FEDERATION_DB_USER"),
-            password=os.getenv("FEDERATION_DB_PASSWORD"),
-            host=os.getenv("FEDERATION_DB_HOST"),
-            port=os.getenv("FEDERATION_DB_PORT"),
-            sslmode='require'   # ✅ <-- This line is the fix
+            dsn=os.getenv("DATABASE_URL"),
+            sslmode='require'
         )
 
     def get_connection(self):
