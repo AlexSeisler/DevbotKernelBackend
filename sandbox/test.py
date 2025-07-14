8fdafc9d5c5bc9af7f89df987d57ccc7902f1a0e
Alex Seisler <alex@acsresultsai.net>
2025-07-13 23:30:00 -0400

    🔁 Expanded patch to shift line count and trigger cache update

--- sandbox/test.py
+++ sandbox/test.py
@@ -19,3 +19,7 @@ def describe1():
     print("Line 3")
     print("Line 4")
     return '🔄 Line-shifting test'
+    print("Line 2")
+    print("Line 3")
+    print("Line 4")
+    return '🔄 Line-shifting test'
