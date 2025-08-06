de05f02585be5c67f5ae15d3b59a2b30db0419cd
Alex Seisler <alex@acsresultsai.net>
2025-08-06 00:19:19 -0400

    Migrate import_repo logic from FederationService to repo_importer module

--- services/federation_service.py
+++ services/federation_service.py
@@ -36,30 +36,6 @@ class FederationService():
         self.proposal_manager = ProposalManager(self.db)
         self.planner = FederatedCSTPatchPlanner()
 
-    def _tag_semantic_node(self, node):
-        tags = []
-
-        name = node.get("name", "")
-        node_type = node.get("node_type", "")
-        decorators = node.get("decorators", [])
-        file_path = node.get("file_path", "")
-
-        if "test" in file_path:
-            tags.append("test")
-        if "infra" in file_path or "ops" in file_path:
-            tags.append("infra")
-        if node_type == "decorator":
-            tags.append("decorator")
-        if name in {"main", "__init__", "run"}:
-            tags.append("entrypoint")
-        if name.startswith("_"):
-            tags.append("internal")
-        if any(k in d for d in decorators for k in ("get", "post", "route")):
-            tags.append("http")
-        if not tags:
-            tags.append("util")
-
-        return tags
 
 
 
