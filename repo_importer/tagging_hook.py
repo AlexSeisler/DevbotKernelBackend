55629647705830b72a1913dcb3ed6baef4b14e6d
Alex Seisler <alex@acsresultsai.net>
2025-08-06 00:27:22 -0400

    Finalize Phase 1 by removing FederationService dependency from repo_importer

--- repo_importer/tagging_hook.py
+++ repo_importer/tagging_hook.py
@@ -1,8 +1,20 @@
 # Wrapper to integrate subsystem_tagging during ingestion
 
 def tag_semantic_node(node):
-    node["subsystem"] = "TODO: tagging logic"
+    node['subsystem'] = infer_subsystem(node)
     return node
+
+
+def infer_subsystem(node):
+    path = node.get('file_path', '')
+    if 'auth' in path:
+        return 'auth'
+    elif 'queue' in path:
+        return 'queue'
+    elif 'worker' in path or 'celery' in path:
+        return 'task'
+    else:
+        return 'core'
 
 def tag_semantic_node(node):
     # Placeholder for tagging logic
