e58713de7d7efb31d3e6dfa2092d1265c809dab1
Alex Seisler <aseisler7045@stevenscollege.edu>
2025-08-06 22:00:04 -0400

    cleanup

--- repo_importer/tagging_hook.py
+++ repo_importer/tagging_hook.py
@@ -36,29 +36,7 @@ class TaggingHook():
         node['tags'] = tags
         node['subsystem'] = self.infer_subsystem(node)
         return node
-        tags = []
-
-        name = node.get("name", "")
-        node_type = node.get("node_type", "")
-        decorators = node.get("decorators", [])
-        file_path = node.get("file_path", "")
-
-        if "test" in file_path:
-            tags.append("test")
-        if "infra" in file_path or "ops" in file_path:
-            tags.append("infra")
-        if node_type == "decorator":
-            tags.append("decorator")
-        if name in {"main", "__init__", "run"}:
-            tags.append("entrypoint")
-        if name.startswith("_"):
-            tags.append("internal")
-        if any(k in d for d in decorators for k in ("get", "post", "route")):
-            tags.append("http")
-        if not tags:
-            tags.append("util")
-
-        return tags
+        
 
     def _tag_all_semantic_nodes(self, nodes):
         for node in nodes:
