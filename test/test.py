858db5478cd140475f75e8dac5f0b26de1d7e4b0
Alex Seisler <alex@acsresultsai.net>
2025-09-15 23:19:27 -0400

    Scaffold test folder and add test.py with FizzBuzz boilerplate

---
+++ test/test.py
@@ -0,0 +1,14 @@
+def fizzbuzz(n: int) -> str:
+    if n % 15 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
+
+if __name__ == "__main__":
+    for i in range(1, 21):
+        print(fizzbuzz(i))
